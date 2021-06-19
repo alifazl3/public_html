8c89eff4d81f57a0960845b38e411bfa918c5ac3
alifazl3 <alifazl3@yahoo.com>
2021-06-19 20:13:26 +0430

    test

--- hook.php
+++ hook.php
@@ -5,22 +5,22 @@ $BOT_TOKEN = "1702194647:AAF1PIcfpjs4CTbqcHYRq6R32mKqpdNmRZ0";
 $my_chatId = "103987269";
 sendMessage($my_chatId, "salam from server");
 
-$update = file_get_contents('php://input');
-$update = json_decode($update, true);
-$userChatId = $update["message"]["from"]["id"]?$update["message"]["from"]["id"]:null;
-
-$userMessage = $update["message"]["text"]?$update["message"]["text"]:"Nothing";
-$firstName = $update["message"]["from"]["first_name"]?$update["message"]["from"]["first_name"]:"N/A";
-$lastName = $update["message"]["from"]["last_name"]?$update["message"]["from"]["last_name"]:"N/A";
-$fullName = $firstName." ".$lastName;
-$replyMsg = "Hello ".$fullName."\nYou said: ".$userMessage;
-
-
-
-
-$text = $replyMsg;
-
-sendMessage($userChatId, $text);
+//$update = file_get_contents('php://input');
+//$update = json_decode($update, true);
+//$userChatId = $update["message"]["from"]["id"]?$update["message"]["from"]["id"]:null;
+//
+//$userMessage = $update["message"]["text"]?$update["message"]["text"]:"Nothing";
+//$firstName = $update["message"]["from"]["first_name"]?$update["message"]["from"]["first_name"]:"N/A";
+//$lastName = $update["message"]["from"]["last_name"]?$update["message"]["from"]["last_name"]:"N/A";
+//$fullName = $firstName." ".$lastName;
+//$replyMsg = "Hello ".$fullName."\nYou said: ".$userMessage;
+//
+//
+//
+//
+//$text = $replyMsg;
+//
+//sendMessage($userChatId, $text);
 
 function sendMessage($chatId, $text)
 {
