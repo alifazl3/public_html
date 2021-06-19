ab2c4131404c22b3f73dda7137483cde5ec29775
alifazl3 <alifazl3@yahoo.com>
2021-06-19 17:01:09 +0430

    god will help me

--- hook.php
+++ hook.php
@@ -4,28 +4,33 @@
 $BOT_TOKEN = "1702194647:AAF1PIcfpjs4CTbqcHYRq6R32mKqpdNmRZ0";
 
 
-$parameters = array(
-    "chat_id" => "103987269",
-    "text" => "test",
-);
-send("sendMessage", $parameters);
+$update = file_get_contents('php://input');
+$update = json_decode($update, true);
+$userChatId = $update["message"]["from"]["id"]?$update["message"]["from"]["id"]:null;
+
+$userMessage = $update["message"]["text"]?$update["message"]["text"]:"Nothing";
+$firstName = $update["message"]["from"]["first_name"]?$update["message"]["from"]["first_name"]:"N/A";
+$lastName = $update["message"]["from"]["last_name"]?$update["message"]["from"]["last_name"]:"N/A";
+$fullName = $firstName." ".$lastName;
+$replyMsg = "Hello ".$fullName."\nYou said: ".$userMessage;
 
 
-function send($method, $data)
+
+
+$text = $replyMsg;
+//$my_chatId = "103987269";
+
+sendMessage($userChatId, $text);
+
+function sendMessage($chatId, $text)
 {
+    global $BOT_TOKEN;
+    $ch = curl_init();
 
-    $url = "https://api.telegram.org/bot1702194647:AAF1PIcfpjs4CTbqcHYRq6R32mKqpdNmRZ0/$method";
-
-    if (!$curld = curl_init()) {
-        exit;
-    }
-    curl_setopt($curld, CURLOPT_POST, true);
-    curl_setopt($curld, CURLOPT_POSTFIELDS, $data);
-    curl_setopt($curld, CURLOPT_URL, $url);
-    curl_setopt($curld, CURLOPT_RETURNTRANSFER, true);
-    $output = curl_exec($curld);
-    curl_close($curld);
-    return $output;
+    $conn = curl_setopt($ch, CURLOPT_URL, "https://api.telegram.org/bot$BOT_TOKEN/sendMessage?chat_id=$chatId&text=$text");
+    $conn = curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    $output = curl_exec($ch);
+    return $conn;
 }
 
 ?>
