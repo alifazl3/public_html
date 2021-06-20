4d015b660fc4b9dc38ac25518dfd77e55c21439a
alifazl3 <alifazl3@yahoo.com>
2021-06-20 13:27:19 +0430

    test

--- hook.php
+++ hook.php
@@ -1,8 +1,51 @@
 <?php
 
 
+function get()
+{
+    $openFile = fopen("gc.txt", "r");
+    $txt = fread($openFile, filesize("gc.txt"));
+    fclose($openFile);
+    return json_decode($txt);
+}
+
+function put($txt)
+{
+    $openFile = fopen("gc.txt", "w");
+    fwrite($openFile, $txt);
+    fclose($openFile);
+}
 
 
+function add($price, $whom)
+{
+    $current_gc = get();
+    $current_gc[$whom]+=$price;
+    put(json_encode($current_gc));
+
+}
+
+
+function sub($price, $whom)
+{
+    $current_gc = get();
+    $current_gc[$whom]=$current_gc[$whom]-$price;
+    put(json_encode($current_gc));
+}
+
+
+function transfer($price, $who, $whom)
+{
+
+    $current_gc = get();
+    $current_gc[$who]=$current_gc[$who]-$price;
+    $current_gc[$whom]=$current_gc[$whom]+$price;
+    put(json_encode($current_gc));
+
+}
+
+
+$current_gc = get();
 
 
 
@@ -32,21 +75,34 @@ $text = json_encode($update);
 
 
 if ($update['message']['text'] == "GetCurrentSt") {
-    $openFile = fopen("gc.txt", "r");
-    $txt = fread($openFile, filesize("gc.txt"));
-    $text = $txt;
-    fclose($openFile);
-}
-if (substr($update['message']['text'],0,3) == "add") {
-    $text = "add verify";
-}
-if (substr($update['message']['text'],0,3) == "sub") {
-    $text = "sub verify";
-}
-if (substr($update['message']['text'],0,3) == "tra") {
-    $text = "tra verify";
+    $text = get();
+
 }
 
+
+if (substr($update['message']['text'],0,3) == "add") {
+
+    $param = explode(' ',$update['message']['text']);
+
+    $text = "add verify".json_encode($param);
+}
+if (substr($update['message']['text'],0,3) == "sub") {
+    $param = explode(' ',$update['message']['text']);
+
+    $text = "sub verify".json_encode($param);
+}
+if (substr($update['message']['text'],0,3) == "tra") {
+    $param = explode(' ',$update['message']['text']);
+
+    $text = "tra verify".json_encode($param);
+}
+
+
+
+
+
+
+put(json_encode($current_gc));
 
 //sendMessage($update['message']['from']['id'], $update['message']['text']);
 sendMessage($update['message']['from']['id'], $text);
