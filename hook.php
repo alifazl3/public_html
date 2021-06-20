2d40062cdc53654be148afbcc39461d92fd94fc7
alifazl3 <alifazl3@yahoo.com>
2021-06-20 11:47:24 +0430

    test

--- hook.php
+++ hook.php
@@ -2,51 +2,40 @@
 
 
 
-//function sendMessage($chatId, $text)
-//{
-//$BOT_TOKEN = "1702194647:AAF1PIcfpjs4CTbqcHYRq6R32mKqpdNmRZ0";
-//
-//    $ch = curl_init();
-//
-//    $conn = curl_setopt($ch, CURLOPT_URL, "https://api.telegram.org/bot$BOT_TOKEN/sendMessage?chat_id=$chatId&text=$text");
-//    $conn = curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-//    $output = curl_exec($ch);
-//    return $conn;
-//}
+function sendMessage($chatId, $text)
+{
+$BOT_TOKEN = "1702194647:AAF1PIcfpjs4CTbqcHYRq6R32mKqpdNmRZ0";
+
+    $ch = curl_init();
+
+    $conn = curl_setopt($ch, CURLOPT_URL, "https://api.telegram.org/bot$BOT_TOKEN/sendMessage?chat_id=$chatId&text=$text");
+    $conn = curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    $output = curl_exec($ch);
+    return $conn;
+}
 
 
 
 $BOT_TOKEN = "1702194647:AAF1PIcfpjs4CTbqcHYRq6R32mKqpdNmRZ0";
 $my_chatId = "103987269";
 
+ 
+
+sendMessage($my_chatId, "salam from server");
+$update = file_get_contents('php://input');
+$update = json_decode($update, true);
+$userChatId = $update["message"]["from"]["id"]?$update["message"]["from"]["id"]:null;
+
+$userMessage = $update["message"]["text"]?$update["message"]["text"]:"Nothing";
+$firstName = $update["message"]["from"]["first_name"]?$update["message"]["from"]["first_name"]:"N/A";
+$lastName = $update["message"]["from"]["last_name"]?$update["message"]["from"]["last_name"]:"N/A";
+$fullName = $firstName." ".$lastName;
+$replyMsg = "Hello ".$fullName."\nYou said: ".$userMessage;
 
 
-//
-    $ch = curl_init();
-//
-//    $conn = curl_setopt($ch, CURLOPT_URL, "https://api.telegram.org/bot$BOT_TOKEN/sendMessage?chat_id=$chatId&text=$text");
-//    $conn = curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-//    $output = curl_exec($ch);
 
 
-//sendMessage($my_chatId, "salam from server");
+$text = $replyMsg;
 
-//$update = file_get_contents('php://input');
-//$update = json_decode($update, true);
-//$userChatId = $update["message"]["from"]["id"]?$update["message"]["from"]["id"]:null;
-//
-//$userMessage = $update["message"]["text"]?$update["message"]["text"]:"Nothing";
-//$firstName = $update["message"]["from"]["first_name"]?$update["message"]["from"]["first_name"]:"N/A";
-//$lastName = $update["message"]["from"]["last_name"]?$update["message"]["from"]["last_name"]:"N/A";
-//$fullName = $firstName." ".$lastName;
-//$replyMsg = "Hello ".$fullName."\nYou said: ".$userMessage;
-//
-//
-//
-//
-//$text = $replyMsg;
-//
-//sendMessage($userChatId, $text);
-
-
+sendMessage($userChatId, $text);
 ?>
