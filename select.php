05d5dcdd5b169051c1277f2e0269219e13fa7f0c
alifazl3 <alifazl3@yahoo.com>
2021-06-21 14:43:12 +0430

    phpmyadmin try 1 and

--- select.php
+++ select.php
@@ -4,33 +4,43 @@
 $servername = "localhost";
 $username = "alifazl";
 $password = "password";
-
 $dbname = "evig";
-
-
+$export = array();
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
 if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
+    die("Connection failed: " . $conn->connect_error);
 }
 
 $sql = "SELECT * FROM gc";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
-  // output data of each row
-  while($row = $result->fetch_assoc()) {
+    // output data of each row
+    while ($row = $result->fetch_assoc()) {
 	var_dump($row);
 	echo "<br>";
-  }
+
+        array_push($export, $row);
+    }
 } else {
-  echo "0 results";
+    echo "0 results";
 }
+
+
 $conn->close();
 
 
+
+echo "<br>";
+echo "<br>";
+echo "<br>";
+
+echo "<pre>";
+var_dump($export);
+echo "</pre>";
 
 
 ?>
