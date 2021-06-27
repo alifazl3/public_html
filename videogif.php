ccc580d11673fb97fcde4c63d4219573fc1ed3d0
alifazl3 <alifazl3@yahoo.com>
2021-06-27 22:04:59 +0430

    update

--- videogif.php
+++ videogif.php
@@ -15,7 +15,7 @@ if (isset($update['inline_query'])) {
 
     $chatId = "103987269";
     $txt = $txt . "%0A";
-    $result = array("type" => "video", "id" => $update['inline_query']['id'], "video_file_id" => "AAMCBAADGQEAAxdg2EeOPd-vpak_GwKjuKugBy1geQACWggAAnC6QVBewCw0brMbLAEAB20AAyAE", "title" => "ok");
+    $result = array("type" => "video", "id" => "-1001165348767", "video_file_id" => "AAMCBAADGQEAAxdg2EeOPd-vpak_GwKjuKugBy1geQACWggAAnC6QVBewCw0brMbLAEAB20AAyAE", "title" => "Videogifs");
     $inlineAnswer = inlineMode($update['inline_query']['id'], json_encode($result));
     $txt = $txt . $inlineAnswer . "%0A%0A%0A" . json_encode($result);
 }
