e32fa985c4ec3b1d2c183133cd1c19937161ef5c
alifazl3 <alifazl3@yahoo.com>
2021-06-28 22:02:48 +0430

    update

--- videogif.php
+++ videogif.php
@@ -17,8 +17,11 @@ if (isset($update['inline_query'])) {
     $txt = $txt . "%0A";
 //    $result = array("type" => "video", "id" => "-1001165348767", "video_file_id" => "AAMCBAADGQEAAxdg2EeOPd-vpak_GwKjuKugBy1geQACWggAAnC6QVBewCw0brMbLAEAB20AAyAE", "title" => "Videogifs");
 //    $result = array('type'=> 'article', 'message_text'=> "salam", 'id'=> '1', 'title'=> 'count');
-    $result = array('type'=> 'article',"id"=> "1", "input_message_content"=> array("message_text"=> "Hello"));
-    $inlineAnswer = inlineMode($update['inline_query']['id'], json_encode($result));
+    $result = array('type' => 'article', "id" => "1", "input_message_content" => array("message_text" => "Hello"));
+
+    $inlineAnswer = do_post(array($update['inline_query']['id'], json_encode($result)));
+
+//    $inlineAnswer = inlineMode($update['inline_query']['id'], json_encode($result));
     $txt = $txt . $inlineAnswer . "%0A%0A%0A" . json_encode($result);
 }
 
@@ -45,7 +48,23 @@ function inlineMode($inline_query_id, $results)
     $conn = curl_setopt($ch, CURLOPT_URL, "https://api.telegram.org/bot$BOT_TOKEN/InlineQuery?inline_query_id=$inline_query_id&results=$results");
     $conn = curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
     $output = curl_exec($ch);
-    return $conn.$output;
+    return $conn . $output;
+}
+
+
+function do_post($params)
+{
+    $BOT_TOKEN = "1723855279:AAGBT_x2M2mspFmbuCg-7_ae7wpri1g0yE8";
+    $url = "https://api.telegram.org/bot$BOT_TOKEN/InlineQuery";
+
+    $options = array(
+        'http' => array(
+            'header' => "Content-type: application/x-www-form-urlencoded\r\n",
+            'method' => 'POST',
+            'content' => $params
+        )
+    );
+    $result = file_get_contents($url, false, stream_context_create($options));
 }
 
 ?>
